3e0f565cdaadeb7f8bdd47f39cae8917f86e71af
Kureo524 <kokoshibo524@gmail.com>
2025-03-18 12:05:51 +0100

    fdiuiufds

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,10 +1,17 @@
 using TMPro;
 using UnityEngine;
 
-public class Node : MonoBehaviour
+public class Node
 {
     public float value;
     public string power;
     public string unity;
     public Node next;
+
+    public Node(float value, string power, string unity)
+    {
+        this.value = value;
+        this.power = power;
+        this.unity = unity;
+    }
 }
